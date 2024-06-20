6ab32df9d9c4a401778906f02470415750e6abfc
Andrés Banda <andrezh_gln7@live.com>
2024-06-19 21:01:04 -0500

    update hero background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,10 +43,10 @@ export default function Home() {
           </div>
           <div className="hidden md:block absolute w-1/2 h-full -right-10 bg-gradient-to-r from-blue-600 to-violet-600">
             <Image
-              src="/back1.jpg"
-              alt="Next.js logo"
+              src="/back2.jpg"
+              alt=""
               fill
-              className="object-cover w-full h-full opacity-90"
+              className="object-cover w-full h-full opacity-80"
             />
             <div className="bg-neutral-200 text-neutral-800 rounded-lg z-10 absolute w-96 bottom-6 -left-20">
               <TestimonialCard testimonial={testimonial} />
