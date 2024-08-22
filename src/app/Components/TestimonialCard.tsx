771165c5c966904ca6da96a3fbdde850ed506375
Andrés Banda <andrezh_gln7@live.com>
2024-08-21 19:23:11 -0500

    add title and remove testimonial card images

--- src/app/Components/TestimonialCard.tsx
+++ src/app/Components/TestimonialCard.tsx
@@ -11,27 +11,9 @@ interface TestimonialCardProps {
 
 const TestimonialCard = ({ testimonial }: TestimonialCardProps) => {
   return (
-    <div className="flex gap-3 shadow p-7">
-      <div className="flex flex-col gap-1">
-        <Image
-          src={testimonial.image}
-          alt="emprendedor"
-          width={100}
-          height={100}
-          className="rounded-full object-cover w-16 h-16"
-        />
-        <div className="flex">
-          {Array(5)
-            .fill(0)
-            .map((_, i) => (
-              <Star className="ml-[1px]" key={i} />
-            ))}
-        </div>
-      </div>
-      <div>
-        <h2 className="text-md font-semibold">{testimonial.name}</h2>
-        <p className="text-sm">{testimonial.description}</p>
-      </div>
+    <div className="bg-neutral-100 p-5 rounded-lg shadow-lg flex flex-col gap-3">
+      <p className="text-sm">{testimonial.description}</p>
+      <h2 className="text-md font-semibold">- {testimonial.name}</h2>
     </div>
   );
 };
--- src/modules/Testimonials.tsx
+++ src/modules/Testimonials.tsx
@@ -5,36 +5,33 @@ const testimonials = [
     name: "María S.",
     image: "/woman-face.jpg",
     description:
-      "Increíble cómo Partners me ayudó a encontrar los compañeros perfectos para mi proyecto tecnológico",
+      "Increíble cómo Partners me ayudó a encontrar los compañeros perfectos para mi proyecto tecnológico.",
   },
   {
-    name: "Carlos L",
+    name: "Carlos L.",
     image: "/entrepreneur-face.jpg",
     description:
-      "Gracias a Partners, nuestro proyecto encontró el experto en IA que necesitábamos para despegar",
+      "Gracias a Partners, encontré al cofundador perfecto para mi startup en solo una semana. Ahora estamos desarrollando un producto que realmente va a revolucionar nuestra industria.",
+  },
+  {
+    name: "Laura R.",
+    image: "/woman-face-2.jpg",
+    description:
+      "Partners me permitió encontrar a un equipo de trabajo con el que pude llevar a cabo mi idea innovadora. ¡Gracias!",
   },
 ];
 
 const Testimonials = () => {
   return (
-    <section className="bg-neutral-200 text-neutral-800 p-5 pt-[4rem] pb-[8rem] max-w-[1280px] mx-auto md:px-[8rem] text-center">
+    <section className="bg-neutral-200 text-neutral-800 p-5 pt-[4rem] pb-[8rem] max-w-[1280px] mx-auto text-center">
       <h2 className="text-3xl font-bold pb-[4rem] text-neutral-800">
         Para gente que quiere cambiar el mundo
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 text-left">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 text-left">
         {testimonials.map((t, i) => (
           <TestimonialCard testimonial={t} key={i} />
         ))}
       </div>
-      <p className="text-lg font-semibold my-10">Descubre tu potencial.</p>
-      <a
-        href="https://forms.gle/S3GReHpDV9CnpxLk6"
-        target="_blank"
-        rel="noreferrer"
-        className="bg-blue-600 text-white px-5 py-3 rounded text-lg font-semibold w-fit mt-5 hover:bg-blue-800 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-      >
-        Empieza hoy
-      </a>
     </section>
   );
 };
